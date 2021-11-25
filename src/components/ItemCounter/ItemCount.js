aaa73f9d723c43ac90450af657587a19d93ac7ca
emilianovaccaro <91967480+emilianovaccaro@users.noreply.github.com>
2021-11-25 01:38:15 -0300

    Correccion Sincronizar ItemCount
    Corrijo el sincro de item count y estados en ItemDetail.

--- src/components/ItemCounter/ItemCount.js
+++ src/components/ItemCounter/ItemCount.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 import { Add, Remove } from '@material-ui/icons'
 import { ProductAmountContainer, ProductAmount } from './styledComponents';
 
 
-const ItemCount = ({stock, initial, onAdd}) => {
+const ItemCount = ({item, stock, initial, updateQty}) => {
   const [quantity, setQuantity] = useState(initial);
-  const [changeBtn, setChangeBtn] = useState(true)
 
   const increment = () => {
     if(quantity < stock ){
@@ -20,31 +18,25 @@ const ItemCount = ({stock, initial, onAdd}) => {
     setQuantity(quantity-1);
     }
   };
-
+/* 
   const addToCart = () => {
     onAdd(quantity);
-    setChangeBtn(false);
-  };
+  }; */
+
+  const onAdd= () => {
+    if(quantity >=1 ){
+      /* addToCart(item, quantity); */
+      updateQty(quantity);
+      setQuantity(0);
+    }
+  }
 
   return(
     <ProductAmountContainer>
-        <Button variant="text" onClick={increment}><Add /></Button>
+      <Button variant="text" onClick={increment}><Add /></Button>
         <ProductAmount>{quantity}</ProductAmount>
-        <Button variant="text" onClick={decrement}><Remove /></Button>
-      {
-        changeBtn
-        ? <Button variant="contained" color="primary" onClick={addToCart}>Add to Cart</Button>
-        : 
-        <div>
-          <Link to="/cart">
-            <Button variant="contained" color="primary">Cart</Button>
-          </Link>
-          <Link to="/">
-            <Button variant="contained" color="secondary">Seguir Comprando</Button>
-          </Link>
-        </div>
-      }
-
+      <Button variant="text" onClick={decrement}><Remove /></Button>
+      <Button variant="contained" color="primary" onClick={onAdd}>Add to Cart</Button>
     </ProductAmountContainer>
   )
 };
